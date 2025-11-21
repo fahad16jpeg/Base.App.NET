877426cde536d7cd5bbe39c0f2baec1702efd7c9
Muhammad Fahad <mfahaddd753@gmail.com>
2025-11-21 12:00:48 +0000

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Vista.App.Data;
+namespace Vista.app.Data
+{
+    public class Category
+    {
+        [Key] // Since the key name (CategoryCode) does not include
+              // "Id", we have to use an annotation (could also
+              // specify this using FluitAPI)
+        [MaxLength(15)]
+        public string CategoryCode { get; set; } = null!;
+        [Required]
+        [MaxLength(30)]
+        public string CategoryName { get; set; } = null!;
+        public List<TrainerCategory>? TrainerCategories { get; set; }
+    }
+}
---
+++ TrainerCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Vista.app.Data;
+namespace Vista.App.Data
+{
+    public class TrainerCategory
+    {
+        // Has a composite (compound) key that will be defined TrainersDbConext
+        [Required]
+        public int TrainerId { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string CategoryCode { get; set; } = null!;
+        public Trainer? Trainer { get; set; }
+        // See TrainersDbConext for Foreign Key (Fluent API) definition
+        public Category? Category { get; set; }
+
+    }
+}
